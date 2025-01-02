0d3dea45cde59557241a6438f507bb9e494750ab
turannul <turanull000@gmail.com>
2025-01-02 20:45:05 +0300

    Create countdown.py

---
+++ countdown.py
@@ -0,0 +1,24 @@
+strings = {
+    "en-US": {
+        "signed": "Signed",
+        "app_name": "App Name",
+        "bundle_id": "Bundle ID",
+        "certificate": "Certificate",
+        "install_button": "📲 Install",
+        "explore_button": "🔎 Discover more",
+        "mnm_notify": "Do you see this because message.edit_text is failed"
+    },
+    "tr-TR": {
+        "signed": "İmzalandı",
+        "app_name": "Uygulama adı",
+        "bundle_id": "Uygulama ID",
+        "certificate": "Sertifika",
+        "install_button": "📲 Yükle",
+        "explore_button": "🔎 Daha fazla iPA",
+        "mnm_notify": "Mesaj duzenlenirken bir hata olustu"
+
+    },
+    "ru-RU": {
+
+    },
+}
