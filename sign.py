092cc1981827dc94152646a7f22f2f88e0994c15
turannul <turanull000@gmail.com>
2025-01-02 20:45:00 +0300

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,9 +1,33 @@
 strings = {
     "en-US": {
-
+        "greeting": "Hello, {message.from_user.first_name}! 😊\nIt seems like you forgot something. Please make your choice and resend the IPA! 🚀",
+        "retry_signing": "An error occurred during signing, retrying... ({failed_sign_attempt}/{max_sign_attempt})",
+        "signing_failed": "Failed to sign.\nThis issue is often caused by the ipa file or Telegram.",
+        "unexpected_error": "An unexpected error occurred.\nIf this issue persists, please reach out to me.",
+        "ipa_ready": "Hey! The IPA has been signed and is ready for upload.",
+        "forgot_choice": "It seems like you forgot something. Please make your choice and resend the IPA!",
+        "file_lost": "File is lost, during signing operation.",
+        "value_error": "Value error during signing operation.",
+        "permission_error": "Permission error during signing operation.",
+        "signing_error": "Signing operation failed.",
+        "unknown_error": "Unexpected error during signing operation.",
+        "select_certificate_button": "Select Certificate 📝️️️️️️",
+        "executing_command": "Executing: {command}"
     },
     "tr-TR": {
-
+        "greeting": "Merhaba, {message.from_user.first_name}! 😊\nSanırım bir şey unuttunuz. Lütfen seçiminizi yapın ve IPA'yı tekrar göndermeyi unutmayın! 🚀",
+        "retry_signing": "İmzalama sırasında hata oluştu, tekrar deneniyor... ({failed_sign_attempt}/{max_sign_attempt})",
+        "signing_failed": "İmzalanamadı.\nBu sorun genellikle ipa'dan veya telegram'dan kaynaklanabilir.",
+        "unexpected_error": "Beklenmedik bir hata oluştu.\nBu sorun sonraki denemenizde düzelmezse lütfen bana yazın.",
+        "ipa_ready": "Hey! iPA imzalandı ve yüklenmeye hazır.",
+        "forgot_choice": "Sanırım bir şey unuttunuz. Lütfen seçiminizi yapın ve IPA'yı tekrar göndermeyi unutmayın!",
+        "file_lost": "File is lost, during signing operation.",
+        "value_error": "Value error during signing operation.",
+        "permission_error": "Permission error during signing operation.",
+        "signing_error": "Signing operation failed.",
+        "unknown_error": "Unexpected error during signing operation.",
+        "select_certificate_button": "Sertifika seç 📝️️️️️️",
+        "executing_command": "Executing: {command}"
     },
     "ru-RU": {
 
