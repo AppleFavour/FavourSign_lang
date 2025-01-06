6e3031ce625f18805b34c821ab7ed72c851ba053
turannul <turanull000@gmail.com>
2025-01-06 15:43:34 +0300

    Create run_cmd_locale.py

---
+++ run_cmd_locale.py
@@ -0,0 +1,23 @@
+strings = {
+    "en-US": {
+        "correct_usage": "Usage: /exec <command>",
+        "admin_only_msg": "Sorry, only admins can execute commands. (Permission denied)",
+        "exec_cmd_exception": "{userID} {username} - Error while executing command: {user_error_message}",
+        "command_out": "Command output:\n{stdout}\n",
+        "command_err": "Command error:\n{stderr}\n"
+    },
+    "tr-TR": {
+        "correct_usage": "Kullanim: /exec <komut>",
+        "admin_only_msg": "Sorry, only admins can execute commands. (Permission denied)",
+        "exec_cmd_exception": "{userID} {username} - Komut calistirirken hata olustu: {user_error_message}",
+        "command_out": ":\n{stdout}\n",
+        "command_err": ":\n{stderr}\n"
+    },
+    "ru-RU": {
+        "correct_usage": "",
+        "admin_only_msg": "",
+        "exec_cmd_exception": "{userID} {username} - : {user_error_message}",
+        "command_out": ":\n{stdout}\n",
+        "command_err": ":\n{stderr}\n"
+    }
+}
