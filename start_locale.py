3c2da6718d9cfd230e01a5d4b53156bd3b74275d
turannul <turanull000@gmail.com>
2025-01-07 15:39:46 +0300

    Create start_locale.py

---
+++ start_locale.py
@@ -0,0 +1,26 @@
+strings = {
+    "en-US": {
+        "welcome_registered": "Hello! {name}\nPlease select a certificate to begin.",
+        "welcome_unregistered": "Hello, {name}!\nSorry, you need admin approval to use the bot.\n\n▸ If you have a certificate, contact us using the button below for approval, or to purchase one if you don't have it.",
+        "select_certificate": "Select certificate 📝️",
+        "select_compression": "Select compression ratio 📚",
+        "select_language": "Select Language 🌐",
+        "approval_purchase": "Get Approval / Purchase"
+    },
+    "tr-TR": {
+        "welcome_registered": "Merhaba! {name}\nBaşlamak için sertifika seçimi yapınız.",
+        "welcome_unregistered": "Merhaba, {name}!\nÜzgünüm, botu kullanabilmeniz için bir yetkilinin onay vermesi gerekiyor.\n\n▸ Sertifikanız varsa onay için, yoksa da satın almak için aşağıdaki butondan bize ulaşabilirsiniz.",
+        "select_certificate": "Sertifika seç 📝️",
+        "select_compression": "Sıkıştırma oranını seç 📚",
+        "select_language": "Dil Seçimi 🌐",
+        "approval_purchase": "Onay / Satın Almak İstiyorum"
+    },
+    "ru-RU": {
+        "welcome_registered": "Привет! {name}\nПожалуйста, выберите сертификат, чтобы начать.",
+        "welcome_unregistered": "Привет, {name}!\nИзвините, для использования бота требуется одобрение администратора.\n\n▸ Если у вас есть сертификат, свяжитесь с нами через кнопку ниже для получения одобрения, или для покупки, если у вас его нет.",
+        "select_certificate": "Выбрать сертификат 📝️",
+        "select_compression": "Выбрать степень сжатия 📚",
+        "select_language": "Выбрать язык 🌐",
+        "approval_purchase": "Получить одобрение / Купить"
+    }
+}
