57f39f9be55ec3bc392e526e78c809bbad3ccc31
turannul <turanull000@gmail.com>
2025-01-07 15:38:35 +0300

    Create speedtest_locale.py

---
+++ speedtest_locale.py
@@ -0,0 +1,17 @@
+strings = {
+    "en-US": {
+        "speedtest_start": "🚀 Testing internet speed...",
+        "network_error": "Network Error: Please check your internet connection and try again.",
+        "timeout_error": "Error: Request timed out.",
+        "unknown_error": "Error: An unexpected error occurred.",
+    },
+    "tr-TR": {
+        "speedtest_start": "🚀 Internet hızı test ediliyor.",
+        "network_error": "Ağ Hatası: Lütfen internet bağlantınızı kontrol edin ve tekrar deneyin.",
+        "timeout_error": "Hata: İstek zaman aşımına uğradı.",
+        "unknown_error": "Hata: Beklenmeyen bir hata oluştu.",
+    },
+    "ru-RU": {
+
+    },
+}
