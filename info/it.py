8a75a05163484fa1cbf6b3b4378bcc72632e504a
Turann_ <turanull000@gmail.com>
2025-10-16 13:19:38 +0300

    Update it.py

--- info/it.py
+++ info/it.py
@@ -3,7 +3,7 @@ class Italian:
         "greeting": "👋 Ciao, {name}!",
         "system_info": "**Informazioni di sistema**",
         "bot_info": "**Informazioni sul Bot**",
-        "bot_stats": "Tempo di attività: {uptime}Utilizzo: {usage}",
+        "bot_stats": "Tempo di attività: {uptime}\nUtilizzo: {usage}",
         "total_users": "Utenti totali: {count}",
         "active_users": "Utenti attivi: {count}",
     }
