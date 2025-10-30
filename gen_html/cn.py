59857a662b0fa49607f68fde7d472bedb503aabe
Turann_ <turanull000@gmail.com>
2025-10-30 04:27:46 +0300

    Rewriting in rust for no particular reason

--- gen_html/cn.py
+++ gen_html/cn.py
@@ -5,7 +5,7 @@ class Chinese:
         "download_button": "下载",
         "selected_certificate": "已选证书",
         "unsupported_system": "不支持的系统",
-        "requiresNewerOSVersion": "需要 iOS {requiredOSVersion} 或更高版本。",,
+        "requiresNewerOSVersion": "需要 iOS {requiredOSVersion} 或更高版本。",
         "chkconnection": "请检查您的网络连接。",
         "connected": "已连接到互联网。",
         "not_connected": "没有互联网连接。",
