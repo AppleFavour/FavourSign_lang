b9e5bdf367e7d433d8a6e41b0f46b4098662be70
Turann_ <turanull000@gmail.com>
2025-11-02 02:05:48 +0300

    rm

--- bot_updater/en.py
+++
@@ -1,6 +0,0 @@
-class English:
-    strings = {
-        "uncommitted_changes": "There are uncommitted changes. Please stash or commit them before updating.",
-        "no_updates": "No updates available.",
-        "update_error": "An error occurred during the update:\n`{error}`",
-    }
--- bot_updater/tr.py
+++
@@ -1,6 +0,0 @@
-class Turkish:
-    strings = {
-        "uncommitted_changes": "Commitlenmemiş değişiklikler var. Güncellemeden önce lütfen stash ya da commit yapın.",
-        "no_updates": "Güncellenecek bir şey yok.",
-        "update_error": "Güncelleme sırasında bir hata oluştu:\n`{error}`",
-    }
